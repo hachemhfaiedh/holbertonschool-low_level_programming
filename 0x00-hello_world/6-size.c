fe9c25f848a8246d273854c93a6d3aef61b93737
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-05 12:53:58 +0000

    low_level_programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @void: empty
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char", sizeof(char));
+printf("Size of an int", sizeof(int));
+printf("Size of a float", sizeof(float));
+printf("Size of a long int", sizeof(long int));
+printf("Size of a long long int", sizeof(long long int));
+return (0);
+}
