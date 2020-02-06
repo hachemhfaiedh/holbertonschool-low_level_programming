e6a2327ccc9f351d3ec079500d897d3fd59e977a
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-06 11:08:51 +0000

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+*main - main function
+*@void: empty
+*function to find out if a variable is positive or negative
+*Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+printf("%d is 0\n", n);
+}
+else
+{
+printf("%d is positive\n", n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - main function
+*@void: empty
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar (c);
+}
+return (0);
+}
