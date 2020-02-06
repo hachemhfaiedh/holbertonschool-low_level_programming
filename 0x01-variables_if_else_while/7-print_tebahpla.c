5522d64e447c8445416d696ae8cad3bd1a67590b
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-06 14:26:30 +0000

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - main function
+*@void: empty
+*Return: 0
+*/
+int main(void)
+{
+char c = 'z';
+char c2 = '\n';
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar (c);
+}
+putchar (c2);
+return (0);
+}
