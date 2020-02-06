3655e04a766f431819667fb8f5f7a5db161ed7a1
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-06 11:25:13 +0000

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/.#1-last_digit.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.2886:1580978743
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,11 @@
 int main(void)
 {
 char c;
+char c2 = '\n';
 for (c = 'a'; c <= 'z'; c++)
 {
 putchar (c);
 }
+putchar (c2);
 return (0);
 }
