49f51aa7676f3d85d1ddf2c07377ae0b2eb47622
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-06 11:31:57 +0000

    variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,21 +16,21 @@ ld = n % 10;
 if (ld > 5)
 {
 printf("Last digit of %d", n);
-printf("is %d", ld);
-printf("and is greater than 5\n");
+printf(" is %d", ld);
+printf(" and is greater than 5\n");
 }
 else if (ld == 0)
 {
 printf("Last digit of %d", n);
-printf("is %d", ld);
-printf("and is 0\n");
+printf(" is %d", ld);
+printf(" and is 0\n");
 
 }
 else
 {
 printf("Last digit of %d", n);
-printf("is %d", ld);
-printf("and is less than 6 and not 0\n");
+printf(" is %d", ld);
+printf(" and is less than 6 and not 0\n");
 }
 return (0);
 }
