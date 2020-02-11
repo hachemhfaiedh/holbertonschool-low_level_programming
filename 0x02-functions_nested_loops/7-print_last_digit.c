fc6a03e59f772cc506cbf8f023ccd5300a956bc5
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-11 08:36:03 +0000

    functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
  * print_last_digit - main function
- *@n: parameter
+ * @n: parameter
  * Return: n % 10
  */
 int print_last_digit(int n)
 {
 if (n < 0)
+{
 n = n * -1;
 _putchar((n % 10) + '0');
+}
 return (n % 10);
 }
