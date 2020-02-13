7874a697e967969ab841d450daa32fe3302aa7e2
Hachem Hfaiedh <hechemhfaiedh@gmail.com>
2020-02-13 11:58:34 +0000

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - main function
+ * @a: parameter
+ * @b: parameter
+ * Return: c
+ */
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - main function
+ * @void: parameter
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+_putchar (i);
+_putchar ('\n');
+return;
+}
