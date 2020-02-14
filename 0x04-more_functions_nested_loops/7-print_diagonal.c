a520973f3a9c2811c25b01db41e469759cd94ffb
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-14 10:51:12 +0000

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_diagonal - main function
+ *@n: parameter
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar (' ');
+}
+_putchar (92);
+_putchar ('\n');
+}
+}
+else
+_putchar ('\n');
+}
