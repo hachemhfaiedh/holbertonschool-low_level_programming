00b773997c9db69c2a84563d0f848fc2e3f088d4
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-18 16:33:07 +0000

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,21 @@
 #include "holberton.h"
+/**
+ * *_strcpy - main function
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-  int i = 0;
-  int u;
-  while (src[i] != '\0')
-    {
-    i++;
-    }
-  for (u = 0; u <= i; u++)
-    {    
+int i = 0;
+int u;
+while (src[i] != '\0')
+{
+i++;
+}
+for (u = 0; u <= i; u++)
+{
 dest[u] = src[u];
-    } 
- return (dest);
+}
+return (dest);
 }
