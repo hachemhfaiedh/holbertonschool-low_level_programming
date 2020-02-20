a0fe2660858b6dd89e6ccf72989e590841235b93
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-20 09:51:41 +0000

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strcat - main function
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j;
+while (dest[i] != '\0')
+i++;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i+j] = src[j];
+}
+return(dest);   
+}
