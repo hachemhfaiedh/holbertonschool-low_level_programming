84bdeee4cef8115e3cfc567523410a7fffe11e74
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-20 13:54:04 +0000

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strncpy - main function
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0, j;
+while (dest[i] != '\0')
+i++;
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[j] = src[j];
+}
+for ( ; j < n; j++)
+{
+dest[j] = '\0';
+}
+return (dest);
+}
