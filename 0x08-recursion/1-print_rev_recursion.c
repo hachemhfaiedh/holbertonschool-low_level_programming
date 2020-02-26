570548aec9b2a3db30e3f796d3fd24fe9c210e0f
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-26 14:24:53 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _print_rec_recursion - main function
+ * _print_rev_recursion - main function
  * @s: parameter
  * Return: void
  */
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
-return;
-_print_rev_recursion (s + 1);
+	return;
+_print_rev_recursion(s + 1);
 _putchar (*s);
 }
