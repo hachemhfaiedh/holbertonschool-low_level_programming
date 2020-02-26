47cf76a56f02207bfe52e5a851d624aa82766d8f
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-26 16:11:48 +0000

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * factorial - main function
+ * @n: parameter
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
