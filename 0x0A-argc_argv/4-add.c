744249317120dbbd57a7fbce089bf09db508a669
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-29 16:10:23 +0000

    argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argv: parameter
+ * @argc: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, c = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		c += atoi(argv[i]);
+		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", c);
+	return (0);
+}
