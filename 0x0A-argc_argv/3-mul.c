a4b12421a6307504a34181c2ab19df485ebc49e4
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-29 15:35:37 +0000

    argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+	char u[100] = "Error";
+	if (argc != 3)
+	{
+		printf("%s\n", u);
+		return (1);
+	}
+	c = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", c);
+	return (0);
+}
