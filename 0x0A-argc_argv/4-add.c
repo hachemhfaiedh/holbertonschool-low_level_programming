54c488f8ec5f770cb0b9d171ce63ba559254cea6
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-02-29 16:53:50 +0000

    argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,22 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1, c = 0;
+	int i, c = 0, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		c += atoi(argv[i]);
-		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
-		}
-		if (argc < 2)
-		{
-			printf("0\n");
-			return (0);
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+		c += atoi(argv[i]);
 	printf("%d\n", c);
 	return (0);
 }
