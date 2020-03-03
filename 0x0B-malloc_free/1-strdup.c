ee6a6c5c175c92ce1cffc94a270faff4cd9c02ac
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-03 14:29:01 +0000

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,10 @@ char *_strdup(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		s = malloc(i + 1);
-	if (s = NULL)
+	if (s == NULL)
 		return (NULL);
 	for (j = 0; j <= i; j++)
-	{
 		s[j] = str[j];
-		s[j] = '\0';
-	}
+	s[j] = '\0';
 	return (s);
 }
