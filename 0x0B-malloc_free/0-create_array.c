d426ad52d63f29bd2d0119df0b355fbdc3f95bf1
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-03 13:26:47 +0000

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *a;
 
 	a = malloc(sizeof(char) * size);
-	if (size <= 0)
-		return ("Null");
+	if (size <= 0 || a == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
