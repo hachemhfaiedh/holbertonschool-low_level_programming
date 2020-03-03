57797b5ef871494b46286d1960556330ff268cf1
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-03 14:15:31 +0000

    malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *_strdup - main function
+ * @str: parameter
+ * Return: s
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, j;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		s = malloc(i + 1);
+	for (j = 0; j < i; j++)
+		s[j] = str[j];
+	s[j] = '\0';
+	return (s);
+}
