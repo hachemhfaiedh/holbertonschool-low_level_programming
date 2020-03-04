7f0c4eb9818cf4eaa50f3a5790b30b94846487ed
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-04 19:14:36 +0000

    malloc_free

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,7 +8,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, f, a = 0;
+	int i, j, f = 0, g = 0;
 	char *one;
 
 	if (ac == 0 || av == NULL)
@@ -26,12 +26,12 @@ char *argstostr(int ac, char **av)
 	{
 		for (j = 0; av[i][j]; j++)
 		{
-			one[a] = av[i][j];
-			a++;
+			one[g] = av[i][j];
+			g++;
 		}
-		one[a] = '\n';
-		a++;
+		one[g] = '\n';
+		g++;
 	}
-	one[a] = '\0';
+	one[g] = '\0';
 	return (one);
 }
