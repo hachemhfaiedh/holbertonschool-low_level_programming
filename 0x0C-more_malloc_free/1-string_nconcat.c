1ab89b478817c9d56cb2c184fbacfc92b45f9392
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-05 13:29:00 +0000

    more_malloc_free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *string_nconcat - main function
+ * @s1: parameter
+ * @s2: parameter
+ * @n: parameter
+ * Return: s
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0, k, l, m = 0;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	s = malloc(n);
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k <= i; k++)
+		s[k] = s1[k];
+	for (l = i; l <= i + n; l++)
+	{
+		s[l] = s2[m];
+		m++;
+		if (n == j)
+			break;
+	}
+	s[l] = '\0';
+	return (s);
+}
