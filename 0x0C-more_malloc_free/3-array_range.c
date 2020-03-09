575b20d9c39901fd6ad2688a0aac4559bc2af8fa
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-09 10:44:40 +0000

    more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,13 +13,12 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	h = malloc(sizeof(int) * (max - min + 1));
+	h = malloc(sizeof(int) * ((max - min) + 1));
 	if (h == NULL)
 		return (NULL);
-	for (i = min; i <= max; i++)
+	for (i = 0; i < ((max - min) + 1); i++)
 	{
-		h[i] = min;
-		min++;
+		h[i] = min + i;
 	}
 	return (h);
 }
