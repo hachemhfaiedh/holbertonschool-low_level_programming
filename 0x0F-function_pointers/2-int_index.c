21dd0af6ce5ccbc2a2313f841a98dda6d84f3a47
hachemhfaiedh <nedhirb@gmail.com>
2020-03-11 18:26:35 +0000

    function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - main function
+ * @array: parameter
+ * @size: parameter
+ * @cmp: parameter
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (0);
+}
