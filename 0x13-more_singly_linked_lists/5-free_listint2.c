98a34adcb77e168185c531c2cd2054258181bcff
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-24 14:43:32 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,17 @@
 #include "lists.h"
-
+/**
+ * free_listint2 - free list
+ * @head: singly linked list
+ * Return: void
+ */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
 
+	if (head == NULL)
+		*head = NULL;
 	while (*head)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		free(*head);
+		*head = (*head)->next;
 	}
 }
