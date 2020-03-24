d443c77eb42b00b75ea998a32932bbae794d068e
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-24 21:57:47 +0000

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head: linked list
+ * @index: index of the deleted node
+ * Return: 1 if successful, -1 if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int c = 0;
+	listint_t *temp = *head, *erased;
+
+	if (!temp)
+		return (-1);
+	if (index != 0)
+	{
+		erased = temp->next;
+		temp->next = erased->next;
+		free(erased);
+		return (1);
+	}
+	else
+	{
+		erased = (*head)->next;
+		free(*head);
+		*head = erased;
+		return (1);
+	}
+	for (c = 0; c < index - 1; c++)
+	{
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+}
