0cf589fb55e986d13dee5ef8af01ead083117485
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-31 19:40:34 +0000

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	while (text_content[i] != '\0')
@@ -20,6 +20,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	E = write(fd, text_content, i);
 	if (E == -1)
 		return (-1);
-	close(fd);
 	return (1);
 }
