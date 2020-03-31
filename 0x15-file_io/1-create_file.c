384e5e3248fb68825437aba1ff90999f0c552424
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-31 19:48:45 +0000

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 	if (write(fd, text_content, i) == -1)
 		return (-1);
 	return (1);
