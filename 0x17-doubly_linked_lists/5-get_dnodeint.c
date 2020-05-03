0f59aed0ca5b21aa66d8d1a342e5c29f37e7f7cc
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-05-03 21:12:27 +0000

    get_dnode

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,10 +11,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head)
 	{
-		a++;
 		if (a == index)
 			return (head);
 		head = head->next;
+		a++;
 	}
 	return (NULL);
 }
