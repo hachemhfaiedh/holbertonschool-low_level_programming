642d3787d4d30f93016471e7becdbd6fbc1277cc
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-05-03 21:09:54 +0000

    get_dnode

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * *get_dnodeint_at_index - returns nth node of a dll
+ * @head: parameter
+ * @index : parameter
+ * Return: head, if the node does not exist, return NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int a = 0;
+
+	while (head)
+	{
+		a++;
+		if (a == index)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
