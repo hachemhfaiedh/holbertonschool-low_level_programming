7eaa68df1b419e325bf6bbb61446f962f5b60009
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-05-03 07:18:05 +0000

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head: parameter
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+
+	if (!head)
+		return;
+	while(tmp != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
