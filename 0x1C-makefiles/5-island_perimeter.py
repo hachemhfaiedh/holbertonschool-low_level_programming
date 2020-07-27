28ac2757d5a3984c59428402b03be304368c73b4
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-07-27 09:39:58 +0000

    Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Island Perimeter Task """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island """
+
+    if not grid:
+        return 0
+
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                for i, j in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                    if (i < 0 or i == len(grid) or
+                       j < 0 or j == len(grid[i]) or grid[i][j] == 0):
+                        per += 1
+    return per
